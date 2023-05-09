77b8bf855560ded763eb708d98f1a9708c253373
agbackhoff <ag.backhoff@gmail.com>
2023-05-08 19:40:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,42 +13,38 @@ import pandas as pd #https://pandas.pydata.org/
 import time #https://docs.python.org/3/library/time.html
 from upload import upload_data
 
-# This is the function that connects to the database, fetches the data, transforms it, and exports it to a .csv file.
+# This function makes the connection to the database, fetches the data, transforms it, and exports it to a .csv file.
 def connect():
-    cnxn = None # We set the connection to None so that we can close it later.
-    try: # Try some code...
-        print('Connecting to the database...') # This is just a print statement to let us know that the script is running.
+    cnxn = None 
+    try: #
+        print('Connecting to the database...') 
         cnxn = pyodbc.connect('DRIVER={###};SERVER=###;DATABASE=###;UID=###;PWD=###') # This is the connection string. You will need to replace the ### with your own credentials.
-        crsr = cnxn.cursor() # This is the cursor that will be used to execute the query: https://github.com/mkleehammer/pyodbc/wiki/Cursor. 
+        crsr = cnxn.cursor() # Documentation: https://github.com/mkleehammer/pyodbc/wiki/Cursor. 
         crsr.execute("SELECT ### FROM ###;") # This is the query that will be executed. You will need to replace the ### with your own query.
-        print('Query executed.') # Test print statement.
-        rows = crsr.fetchall() # This fetches all the rows from the query (without the column names).
-        # the 'rows' variable is a list of tuples. Each tuple is a row from the query results.
-        print('Query results fetched.') # Test print statement.
-        csv_data = [",".join(map(str, row)) for row in rows] # This transforms the query results into a list of strings, each string representing a row.
+        print('Query executed.')
+        rows = crsr.fetchall() # Fetches all the rows from the query as a list of tuples.
+        print('Query results fetched.')
+        csv_data = [",".join(map(str, row)) for row in rows] # Transforms the list of tuples into a list of strings, each string representing a row.
         print('List of tuples transformed into list of strings.')
-        split_data = [row.split(",") for row in csv_data] # This transforms the list of strings into a list of lists, each list representing a row, each column separated by a comma.
+        split_data = [row.split(",") for row in csv_data] # Transforms the list of strings into a list of lists, each list representing a row, each column separated by a comma.
         print('List of strings transformed into list of lists.')
-        df = pd.DataFrame(split_data, columns=['###', '###']) # Here we use pandas to turn the list of lists into a dataframe (add as many column names separated by commas as needed).
+        df = pd.DataFrame(split_data, columns=['###', '###']) # Replace '###' with the corresponding column names.
         print('List of lists transformed into dataframe.')
-        df.to_csv("#/###/###.csv", index=False) # Here we use pandas to export the dataframe to a csv file, you will need to replace the ### with your own path.
-        print('OUTPUT FILE GENERATED') # Test print statement.
-        crsr.close() # This closes the cursor.
+        df.to_csv("#/###/###.csv", index=False) 
+        print('OUTPUT FILE GENERATED')
+        crsr.close()
 
-    # This exception is only run if needed...
-    except(Exception, pyodbc.Error) as error: # This is the exception handling given by pyodbc: https://github.com/mkleehammer/pyodbc/wiki/Exceptions.
+    except(Exception, pyodbc.Error) as error: # Exception handling by pyodbc: https://github.com/mkleehammer/pyodbc/wiki/Exceptions.
         print(error)
 
-    finally: # This finally statement always runs...
-        if cnxn is not None: # This runs if the connection is not 'None'.
-            cnxn.close() # This closes the connection.
-            print('Database connection terminated.') # Test print statement.
-            print('Initiating upload to BigQuery.') # Test print statement.
-            upload_data() # This calls the function from the upload.py file.
+    finally: 
+        if cnxn is not None:
+            cnxn.close()
+            print('Database connection terminated.')
+            print('Initiating upload to BigQuery.')
+            upload_data() # Calls the function from the upload.py file.
 
-while True: # This while loop allows the script to run every (x) amount of time.
+while True:
     print("Los datos seran actualizados cada 5 min.")
-    
-    connect() # This runs the connect() function declared in this file (main.py).
-
-    time.sleep(300) # Default is 300 seconds (5 minutes). You can change this to whatever you want.
+    connect()
+    time.sleep(300) # 300 seconds (5 minutes). Change this as needed.
